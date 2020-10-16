08d7353df4426623715a34ffda53f37f55c87f93
marktennyson <marktennyson1@yahoo.com>
2020-10-16 16:47:23 +0530

    added ejs file renderer and css stylesheet

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const { PayloadMaker } = require('./components/PayloadMaker')
+// const ejs = require('ejs')
 const app = express()
 const PORT = 5000
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(express.static('static'))
 
 app.get('/',(req,res) =>{
     res.status(200)
     // let data = PayloadMaker({name:"Aniket_Sarkar"})
     // res.send({message:"This is the index page!", data:data})
-    res.sendFile(__dirname+'/templates/index.html')
+    res.render(__dirname+'/templates/index.ejs')
 })
 
 app.post('/url-shortener', (req,res) =>{
