54efe19908d5a2d0666e386709900678a0c06205
marktennyson <marktennyson1@yahoo.com>
2020-10-17 15:06:30 +0530

    api endpoint renamed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const { PayloadMaker } = require('./components/PayloadMaker')
 const app = express()
 const PORT = 5000
-const _api = "http://localhost:8000/v1/"
+const _api = "https://kpqitanvcb.execute-api.ap-south-1.amazonaws.com/api/v1/"
 const headers = { "Content-Type": "application/json" }
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -18,7 +18,6 @@ app.get('/',(req,res) =>{
 app.get('/:short_url', (req, res) => {
     let short_url = req.params.short_url
     let payload = PayloadMaker({short_url:short_url})
-    console.log(req.body)
     let url = _api+'url-short-to-long'
     fetch(url, {
         method: 'PUT',
@@ -26,7 +25,6 @@ app.get('/:short_url', (req, res) => {
         headers: headers
     })
     .then(resp => resp.json()).then(jsonData => {
-        console.log(jsonData)
         if (!jsonData.long_url){res.send({message: "Invalid short URL"})}
         res.redirect(jsonData.long_url)
     })
